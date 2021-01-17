5c3e726a90d99a1e39b16976df7abfb12f610d28
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-17 06:59:15 -0600

    added a handleClick function to the button on home.js; for now, it is to understand the event object; will change to make it functional

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (event) => {
+    console.log('Haayyy', event.target);
+  };
+
+  // the 'event' object or parameter
+  // automatic access inside a function when the event occurs
+  // when you reference the function
+  // its first automatic parameter it receives is the event object
+
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 export const navStyles = {
   container:
-    'flex items-center my-0 mx-auto p-5 bg-gray-100 rounded-b-sm max-w-screen-sm',
+    'flex items-center my-0 mx-auto p-5 bg-gray-50 border border-gray-100 rounded-b-sm max-w-screen-sm',
   header: 'font-semibold text-pink-400',
   links: 'ml-auto',
   link: 'ml-4 p-1.5 no-underline hover:text-pink-400',
